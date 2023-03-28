d8eba22833060848e9782b6dbe92896f480a445c
Abishek Balaji <abishekbalaji7.151@gmail.com>
2023-03-28 20:12:23 +0530

    Imported assets and set up responsive navbar u ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import { Article } from "./components";
+import {
+  Header,
+  Features,
+  Blog,
+  Footer,
+  Possibility,
+  WhatGPT,
+} from "./containers";
+
+import { CTA, Brand, Navigation } from "./components";
+
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-      <Article />
+    <div className="App">
+      <div className="gradient__bg">
+        <Navigation />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 };
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,9 +1,71 @@
+import { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import logo from "../../assets/logo.svg";
+
 import "./Navigation.css";
 
+const MENU = () => (
+  <>
+    <p>
+      <a href="#home">Home</a>
+    </p>
+    <p>
+      <a href="#wgpt3">What is GPT?</a>
+    </p>
+    <p>
+      <a href="#possibility">Open AI</a>
+    </p>
+    <p>
+      <a href="#features">Case Studies</a>
+    </p>
+    <p>
+      <a href="#blog">Library</a>
+    </p>
+  </>
+);
+
 const Navigation = () => {
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
   return (
-    <div>
-      <h1>Navigation</h1>
+    <div className="gpt__navigation">
+      <div className="gpt__navlinks">
+        <div className="gpt__navlinks_logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="gpt__navlinks_container">
+          <MENU />
+        </div>
+      </div>
+      <div className="gpt__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+      <div className="gpt__navbar-menu">
+        {isMenuVisible ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setIsMenuVisible(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setIsMenuVisible(true)}
+          />
+        )}
+        {isMenuVisible && (
+          <div className="gpt__navbar-menu_container scale-up-center">
+            <div className="gpt__navbar-menu_container-links">
+              <MENU />
+              <div className="gpt__navbar-menu_container-links-sign">
+                <p>Sign in</p>
+                <button type="button">Sign up</button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };

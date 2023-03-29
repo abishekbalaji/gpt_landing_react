5782d32b06eb9a8dc679a5dac869a47ac0297c7b
Abishek Balaji <abishekbalaji7.151@gmail.com>
2023-03-29 09:16:56 +0530

    Wired up header UI

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -1,9 +1,33 @@
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
+
 import "./Header.css";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="gpt__header section__padding">
+      <div className="gpt__header-content">
+        <h1 className="gradient__text">
+          Let's Build Something Amazing with GPT OpenAI
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <div className="gpt__header-content__input">
+          <input type="email" placeholder="Your EMail Address" />
+          <button type="button">Get Started</button>
+        </div>
+        <div className="gpt__header-content__people">
+          <img src={people} alt="people" />
+          <p>1600 people requested access to visit in last 24 hours</p>
+        </div>
+      </div>
+      <div className="gpt__header-image">
+        <img src={ai} alt="AI" />
+      </div>
     </div>
   );
 };

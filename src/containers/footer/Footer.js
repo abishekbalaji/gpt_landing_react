722d96448da126545289102b8d4b26ef36be8ee7
Abishek Balaji <abishekbalaji7.151@gmail.com>
2023-03-30 22:54:38 +0530

    Wired up footer section UI

--- src/containers/footer/Footer.js
+++ src/containers/footer/Footer.js
@@ -1,9 +1,46 @@
+import gptLogo from "../../assets/logo.svg";
+
 import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="gpt__footer section__padding">
+      <div className="gpt__footer-heading">
+        <h1 className="gradient__text">
+          Do you want to step in to the future before others
+        </h1>
+      </div>
+      <div className="gpt__footer-button">
+        <p>Request Early Access</p>
+      </div>
+      <div className="gpt__footer-links">
+        <div className="gpt__footer-links_logo">
+          <img src={gptLogo} alt="logo" />
+          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+        </div>
+        <div className="gpt__footer-links_div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt__footer-links_div">
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Reach Out</p>
+        </div>
+        <div className="gpt__footer-links_div">
+          <h4>Get in touch</h4>
+          <p>Crechterwoord K12 182 DK Alknjkcb</p>
+          <p>085-132567</p>
+          <p>info@gpt.net</p>
+        </div>
+      </div>
+      <div className="gpt__footer-copyright">
+        <p>© 2023 GPT-3. All rights reserved.</p>
+      </div>
     </div>
   );
 };

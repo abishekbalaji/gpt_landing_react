922a7e57d48e667f39d2a211352135224f81a3df
Abishek Balaji <abishekbalaji7.151@gmail.com>
2023-03-30 20:06:34 +0530

    Wired up call to action section UI

--- src/components/CTA/CTA.js
+++ src/components/CTA/CTA.js
@@ -2,8 +2,14 @@ import "./CTA.css";
 
 const CTA = () => {
   return (
-    <div>
-      <h1>CTA</h1>
+    <div className="gpt__cta">
+      <div className="gpt__cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className="gpt__cta-button">
+        <button type="button">Get Started</button>
+      </div>
     </div>
   );
 };
